dd94ae3143829c14bab64c90dca2f56b495cf60c
Aaryan <a.k.65983.586@gmail.com>
2024-06-13 13:18:24 +0530

    First model created for Shop Application and registered it in admin.py file

--- Ekart/shop/views.py
+++ Ekart/shop/views.py
@@ -4,3 +4,21 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, 'shop/index.html')
+
+def about(request):
+    return HttpResponse("about page")
+
+def contact(request):
+    return HttpResponse("Contact Page")
+
+def tracker(request):
+    return HttpResponse("tracker page")
+    
+def search(request):
+    return HttpResponse("search page")
+
+def productview(request):
+    return HttpResponse("productview page")
+
+def checkout(request):
+    return HttpResponse("checkout page")
