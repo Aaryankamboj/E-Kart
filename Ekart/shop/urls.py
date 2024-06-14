c4262d24643e18d229e2230eb6c16938c5b7ca27
Aaryan <a.k.65983.586@gmail.com>
2024-06-14 18:27:48 +0530

    learnt about django shell and models

--- Ekart/shop/urls.py
+++ Ekart/shop/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path("", views.index, name="ShopHome"),
+    path("", views.index, name="shop"),
     path("about/", views.about, name="about"),
     path("contact/", views.contact, name="contact"),
     path("tracker/", views.tracker, name="tracker"),
     path("search/", views.search, name="search"),
     path("productview/", views.productview, name="productview"),
     path("checkout/", views.checkout, name="checkout"),
+    path("home/", views.home, name="home"),
+    path("basic/", views.basic, name="basic"),
 ]
--- Ekart/shop/views.py
+++ Ekart/shop/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Product
 # Create your views here.
+
+def home(request):
+    params={
+        'product':Product.objects.all()
+    }
+    return render(request, 'shop/home.html', params)
 
 def index(request):
     return render(request, 'shop/index.html')
+
+
+def basic(request):
+    return render(request, 'shop/basic.html')
 
 def about(request):
     return HttpResponse("about page")
